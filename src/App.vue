7dbb440ea4ca9e70f891daa8b2923deb95d625a9
FAIRDEVX <115607341+FAIRDEVX@users.noreply.github.com>
2023-08-17 20:19:22 +0700

    update
    การใช้ v-for

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import LernBasic from './components/LernBasic.vue';
+
 </script>
 
 <template>
@@ -12,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <LernBasic/>
 </template>
 
 <style scoped>
---
+++ src/components/LernBasic.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div class="my-profile-card">
+        <p>My Name is: {{ name }}</p>
+        <p>My Nickname: {{ nickname }}</p>
+        <p>I'm: {{ age }} year old.</p>
+        <p>My Position: {{ position }}</p>
+        <p>My favourite movies is</p>
+        <li v-for="myFavMovies in myFavMovies">{{ myFavMovies.name }}</li>
+        <button class="btn" @click="count++">Click Me {{ count }}</button>
+        <button class="btn" @click="name ='Meebwow'">Change my name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "Sopat Phanthusuwan",
+            nickname: "Fair",
+            age: "2x",
+            position: "FontEnd Developer",
+            myFavMovies: [
+                {
+                    name: 'Fast and Furious'
+                },
+                {
+                    name: 'Spider Man'
+                },
+                {
+                    name: 'Doctor Strange '
+                },
+            ],
+            count: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+.my-profile-card {
+    color: black;
+    background: pink;
+    border-radius: 5px;
+    box-shadow: 3px 3px 10px rgb(0, 0, 0, 0.3);
+    padding: 1rem;
+}
+
+.btn {
+    margin: 1rem;
+}
+</style>
