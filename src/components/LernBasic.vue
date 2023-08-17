9d9ace9e000e34f8f507cd29cc95d5e8a7085d4d
FAIRDEVX <115607341+FAIRDEVX@users.noreply.github.com>
2023-08-17 21:05:48 +0700

    Update LernBasic.vue

--- src/components/LernBasic.vue
+++ src/components/LernBasic.vue
@@ -1,13 +1,18 @@
 <template lang="">
     <div class="my-profile-card">
-        <p>My Name is: {{ name }}</p>
-        <p>My Nickname: {{ nickname }}</p>
-        <p>I'm: {{ age }} year old.</p>
-        <p>My Position: {{ position }}</p>
-        <p>My favourite movies is</p>
-        <li v-for="myFavMovies in myFavMovies">{{ myFavMovies.name }}</li>
+        <p>My Name is : {{ name }}</p>
+        <p>My Nickname : {{ nickname }}</p>
+        <p>I'm : {{ age }} year old.</p>
+        <p>My Position : {{ position }}</p>
+        <p>My favourite movies is :</p>
+        <li v-for="myFavMovies in myFavMovies">{{ myFavMovies }}</li>
         <button class="btn" @click="count++">Click Me {{ count }}</button>
         <button class="btn" @click="name ='Meebwow'">Change my name</button>
+
+        <button @click="awesome = !awesome">Toggle</button>
+        <p v-if="awesome">Hello World !!</p>
+        <p v-else>Hello Human !!</p>
+
     </div>
 </template>
 
@@ -19,18 +24,9 @@ export default {
             nickname: "Fair",
             age: "2x",
             position: "FontEnd Developer",
-            myFavMovies: [
-                {
-                    name: 'Fast and Furious'
-                },
-                {
-                    name: 'Spider Man'
-                },
-                {
-                    name: 'Doctor Strange '
-                },
-            ],
-            count: 0
+            myFavMovies: ["Fast and Furious", "Spider Man", "Doctor Strange "],
+            count: 0,
+            awesome: true
         }
     }
 }
